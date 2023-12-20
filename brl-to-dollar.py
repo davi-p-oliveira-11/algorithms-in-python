9b5d959495a06832378de7f0ab2f219579c46d44
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 00:50:41 -0300

    Update brl-to-dollar.py

--- brl-to-dollar.py
+++ brl-to-dollar.py
@@ -2,3 +2,8 @@
 9) Create an algorithm that reads how much money a person has in their wallet
  (in R$) and shows how many dollars they can buy. Consider US$1.00 = R$3.45.
 '''
+
+availableBRL = float(input('Enter a value in BRL: ' ))
+valueUSD = availableBRL / 3.45
+
+print(f'You can buy a total of {valueUSD:.2f} USD')
