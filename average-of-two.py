e2177a985ad0701b3521ea4af99aebdb0777279f
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-19 23:51:53 -0300

    Create average-of-two.py

---
+++ average-of-two.py
@@ -0,0 +1,7 @@
+'''
+5) Create a program that reads a student's two grades in a subject and displays their average on the screen.
+Example:
+Grade 1: 4.5
+Grade 2: 8.5
+The average between 4.5 and 8.5 is equal to 6.5.
+'''
