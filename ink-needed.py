b6de5cdec1519fee477ed51bba91afbc7a054a82
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 00:51:30 -0300

    Update ink-needed.py

--- ink-needed.py
+++ ink-needed.py
@@ -4,3 +4,12 @@
  and the amount of paint needed for the job,
  considering that each liter of paint covers an area of 2 square meters.
 '''
+
+height = float(input('Enter the height of the wall in meters: '))
+width = float(input('Enter the width of the wall in meters: '))
+
+area = width * height
+amountNeeded = area * 0.5
+
+print(f'The area of the wall to be painted is {area} square meters.')
+print(f'and the amount of paint needed to paint the wall is {amountNeeded} liters.')
