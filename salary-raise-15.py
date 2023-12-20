51ed834337719731008a40038f2b5319fe751ad5
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 02:20:32 -0300

    Update salary-raise-15.py

--- salary-raise-15.py
+++ salary-raise-15.py
@@ -2,3 +2,10 @@
  13) Create an algorithm that reads an employee's salary,
  calculates, and displays their new salary with a 15% increase."
 '''
+
+salary = float(input('Enter the value of your salary: ' ))
+
+salary_raise = salary * 0.15
+new_salary = salary + salary_raise
+
+print(f'The value of the new salary is {new_salary}')
