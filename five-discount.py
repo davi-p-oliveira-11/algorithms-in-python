94acb9cf2b6ee8358997442cbbc56b7df76ddd02
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 02:19:10 -0300

    Update five-discount.py

--- five-discount.py
+++ five-discount.py
@@ -2,3 +2,11 @@
  12) Create a program that reads the price of a product,
  calculates, and displays its PROMOTIONAL PRICE with a 5% discount.
 '''
+
+product_price = float(input('Enter the price of the product: ' ))
+
+discount = product_price * 0.05
+final_price = product_price - discount
+
+print(f'The original price of the product is {product_price}')
+print(f'The price with discount is {final_price}')
