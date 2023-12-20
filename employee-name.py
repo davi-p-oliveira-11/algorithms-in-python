50381a81ecfb7541d9bfb8490cc4ce7549b3a1e7
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-19 23:47:54 -0300

    Create employee-name.py

---
+++ employee-name.py
@@ -0,0 +1,7 @@
+'''
+3) Create a program that reads the name and salary of an employee, displaying a message at the end.
+Example:
+Employee's Name: Jane Doe
+Salary: 1850.45 USD
+Employee Jane Doe has a salary of R$1850.45 in June.
+'''
