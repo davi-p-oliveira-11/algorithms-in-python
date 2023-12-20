ec76f19b1e9f0bbcbb8afa22eb43f72c92174256
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 00:51:06 -0300

    Create ink-needed.py

---
+++ ink-needed.py
@@ -0,0 +1,6 @@
+'''
+10) Create an algorithm that reads the width and height of a wall,
+ calculates and displays the area to be painted,
+ and the amount of paint needed for the job,
+ considering that each liter of paint covers an area of 2 square meters.
+'''
