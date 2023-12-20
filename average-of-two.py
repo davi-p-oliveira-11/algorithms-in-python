a16f01fb5cb353c0f075c0c526ac4df74587bf69
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-19 23:52:22 -0300

    Update average-of-two.py

--- average-of-two.py
+++ average-of-two.py
@@ -5,3 +5,10 @@ Grade 1: 4.5
 Grade 2: 8.5
 The average between 4.5 and 8.5 is equal to 6.5.
 '''
+
+grade_1 = float(input('Enter the first grade: '))
+grade_2 = float(input('Enter the second grade: '))
+
+average = (grade_1 + grade_2) / 2
+
+print(f'The average between {grade_1} and {grade_2} is equal to {average}')
