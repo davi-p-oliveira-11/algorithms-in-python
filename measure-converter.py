ff0ad0c540750fa2b355527cc90ce09ebb583767
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 00:01:58 -0300

    Create measure-converter.py

---
+++ measure-converter.py
@@ -0,0 +1,13 @@
+'''
+8) Develop a program that reads a distance in meters and shows the
+corresponding values in other units.
+Example:
+Enter a distance in meters: 185.72
+The distance of 185.72m corresponds to:
+0.18572Km
+1.8572Hm
+18.572Dam
+1857.2dm
+18572.0cm
+185720.0mm
+'''
