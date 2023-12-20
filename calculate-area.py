aba0b6d23854798bff888abf144978716c2dc996
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 01:27:16 -0300

    Update calculate-area.py

--- calculate-area.py
+++ calculate-area.py
@@ -3,3 +3,10 @@
  from a quadratic equation and displays the value of Delta.
 '''
 
+value_a = int(input('Enter the value of A '))
+value_b = int(input('Enter the value of B '))
+value_c = int(input('Enter the value of C '))
+
+#bhaskara:  delta = b² - 4ac
+value_delta = (value_b ** 2) - 4 * (value_a * value_c)
+print(f'O valor de delta equivale a {value_delta}')
