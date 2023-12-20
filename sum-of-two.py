1607c0b10a154509763d430306badab19bbf859f
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-19 23:49:19 -0300

    Update sum-of-two.py

--- sum-of-two.py
+++ sum-of-two.py
@@ -5,3 +5,10 @@ Enter a value: 8
 Enter another value: 5
 The sum of 8 and 5 is equal to 13.
 '''
+
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter another number: '))
+
+sum = num_1 + num_2
+
+print(f'The sum between {num_1} and {num_2} is {sum}')
