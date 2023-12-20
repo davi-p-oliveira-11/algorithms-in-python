380272783ddedba87d6cd18bdf6678e66fdd47c0
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-20 00:02:22 -0300

    Update measure-converter.py

--- measure-converter.py
+++ measure-converter.py
@@ -11,3 +11,22 @@ The distance of 185.72m corresponds to:
 18572.0cm
 185720.0mm
 '''
+
+meters = float(input('Enter a distance in meters '))
+
+kilometers = meters / 1000
+hectometers = meters / 100
+decameters = meters / 10
+decimeters = meters * 10
+centimeters = meters * 100
+millimeters = meters * 1000
+
+print(f'''
+ The distance of {meters:.2f}m ccorresponds to:
+{kilometers}Km
+{hectometers}Hm
+{decameters}Dam
+{decimeters}dm
+{centimeters}cm
+{millimeters}mm
+''')
